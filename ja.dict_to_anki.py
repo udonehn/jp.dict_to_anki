7ded018d9e057eacb5c0fb332cc3a1eba63511ae
udonehn <udonehn@gmail.com>
2022-12-09 14:46:40 +0900

    원어 추가

--- ja.dict_to_anki.py
+++ ja.dict_to_anki.py
@@ -6,40 +6,43 @@ import time
 import pyperclip
 import os
 
-
-url = 'https://learn.dict.naver.com/wordbook/jakodict/#/my/cards?wbId=9787c6d9d1be48a7abd02c6bffc1aa3b&qt=0&st=0&name=%EB%8B%A8%EC%96%B4&tab=list'
+url = 'https://learn.dict.naver.com/wordbook/jakodict/#/my/cards?wbId=9787c6d9d1be48a7abd02c6bffc1aa3b&qt=0&st=0&name=단어&tab=list'
 user_id = (input("네이버 아이디 입력 : "))
 user_pw = (input("네이버 비밀번호 입력 : "))
-num=int(input("총 페이지 수 입력 : "))   #페이지 수 설정
+num = int(input("총 페이지 수 입력 : "))   #페이지 수 설정
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(options=options)
 
-
-
 driver.get(url)
-driver.implicitly_wait(3)
+driver.implicitly_wait(3)   #3초안에 웹페이지를 load 하면 바로 넘어가거나, 3초를 기다림
 
 id = driver.find_element(By.ID, 'id')
 id.click()
-pyperclip.copy(user_id)
-id.send_keys(Keys.CONTROL, 'v')
-time.sleep(1)
+pyperclip.copy(user_id) #user_id 변수 복사
+id.send_keys(Keys.CONTROL, 'v') #붙여넣기
 
 pw = driver.find_element(By.ID, 'pw')
 pw.click()
 pyperclip.copy(user_pw)
 pw.send_keys(Keys.CONTROL, 'v')
+
+driver.find_element(By.XPATH, '//*[@id="log.login"]').click()   #로그인 버튼 클릭
 time.sleep(1)
 
-driver.find_element(By.XPATH, '//*[@id="log.login"]').click()
-time.sleep(1)
-driver.find_element(By.ID, 'new.dontsave').click()
+try:    #처음 등록하는 기기일 시 "등록 안함" 클릭
+    driver.find_element(By.ID, 'new.dontsave').click()
+    time.sleep(1)
+except:
+    pass
 
-file = open(os.path.join(os.path.expanduser('~'),'Desktop\\')+'word_anki.txt', 'w',encoding='UTF-8')  #저장할 파일 열기 (바탕화면)
-for i in range(num):    #페이지 수 만큼 실행
-    html = driver.page_source
+file = open(os.path.join(os.path.expanduser('~'),'Desktop\\')+'word_anki.txt', 'w', encoding='UTF-8')  #저장할 파일 열기 (바탕화면)
+
+page = 1
+
+while True:
+    html = driver.page_source   #driver가 위치한 웹페이지의 소스 코드
     soup = BeautifulSoup(html,'html.parser')    #html parser 실행
     divs = soup.findAll('div', 'inner_card')    #단어 목록을 div단위로 추출 (한 페이지에 20개)
 
@@ -55,8 +58,8 @@ for i in range(num):    #페이지 수 만큼 실행
         for meaning in meanings:
             if meaning.em is not None: #값이 none이 아니라면 실행
                 meaning.em.decompose() #em 태그 제거  
-            file.write(meaning.get_text().replace('\n',"").replace('	',"")+'<br>') #뜻에서 텍스트 추출, 엔터 제거, 공백(탭?)제거 + 줄바꿈
-            
+            file.write(meaning.get_text().replace('\n',"").replace('	',"")+'<br>') #뜻에서 텍스트 추출, 엔터 제거, 공백(탭?)제거 + 줄바꿈   
+        
         file.write('\t')
         
         exams = div.findAll('li','item_example')    #단어의 모든 예문 찾기
@@ -65,13 +68,22 @@ for i in range(num):    #페이지 수 만큼 실행
             file.write(origin.get_text().replace('\n',"").replace('	',"")+'<br>') 
             translate = exam.find('p','translate')  #예문 해석 추출
             file.write(translate.get_text().replace('\n',"").replace('	',"")+'<br><br>')
-
+        
+        try:    #참고어가 있다면 실행
+            refer = div.find('span', 'title_origin')    #참고어 찾기
+            file.write('\t')
+            file.write(refer.get_text())
+        except:
+            pass
+                
         file.write('\n')
         
-    driver.find_element(By.CLASS_NAME, 'btn.btn_next._next_page_btn').click()
+    if page == num: #마지막 페이지에 도달하면 break
+        break
+    driver.find_element(By.CLASS_NAME, 'btn.btn_next._next_page_btn').click()   #다음 페이지 버튼 클릭
+    page+=1 #페이지 카운트
     time.sleep(1)
 
 file.close()
-
 print("끝")
 time.sleep(60)
